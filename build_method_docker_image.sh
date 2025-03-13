7972547aef25df60c795196a3f3ec497ab2b3e20
黄照样 <554598474@qq.com>
2025-03-13 17:21:48 +0800

    workflow test: Build Method Docker Image

---
+++ build_method_docker_image.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# 定义变量
+method_name="${1:-cf_paga}"
+destination_dir="method_docker"
+
+# 定义源文件和目标文件路径
+source_parse_file="cfe/method/function/parse_args.py"
+source_method_file="cfe/method/function/${method_name}.py"
+source_dockerfile="cfe/method/Dockerfile/${method_name}.dockerfile"
+source_definition="cfe/method/definition/${method_name}.yml"
+
+destination_parse_file="${destination_dir}/parse_args.py"
+destination_method_file="${destination_dir}/run.py"
+destination_dockerfile="${destination_dir}/Dockerfile"
+destination_definition="${destination_dir}/definition.yml"
+
+# 创建目标目录（如果不存在）
+mkdir -p "${destination_dir}"
+
+# 复制并重命名文件
+cp "${source_parse_file}" "${destination_parse_file}"
+cp "${source_method_file}" "${destination_method_file}"
+cp "${source_dockerfile}" "${destination_dockerfile}"
+cp "${source_definition}" "${destination_definition}"
+echo "Files have been copied and renamed successfully."
+
+# 提取版本号
+version=$(grep -oP '(?<=version: ).*' "${destination_definition}")
+echo "Extracted version: ${version}"
+
+# 构建,、测试、发布Docker 镜像
+image_name="huangzhaoyang/${method_name}:${version}"
+docker build -t ${image_name} "./${destination_dir}"
+echo "Docker image:${image_name} built successfully."
+
+docker run \
+    -it --rm \
+    -v ./method_docker_input:/data \
+    --workdir /code \
+    ${image_name}\
+    python /code/run.py --adata_path /data/adata.h5ad --prior_information /data/prior_information.json --parameters /data/parameters.json --output_filename /data/output.pkl
+echo "Docker image:${image_name} test successfully."
+
+# echo "Docker image:${image_name} push successfully."
---
+++ method_docker/run.py
@@ -0,0 +1,96 @@
+import pickle
+
+import numpy as np
+import pandas as pd
+import anndata as ad
+import scanpy as sc
+import scvelo as scv
+
+
+def cf_paga(
+    adata: ad.AnnData,
+    prior_information: dict = {},
+    parameters: dict = {}
+):
+
+    # 1. prepare data
+    adata = adata.copy()
+    # extract prior information and parameters
+    start_id = prior_information["start_id"]
+    connectivity_cutoff = parameters.get("connectivity_cutoff", 0.5)
+    cluster_key = "cf_paga_clusters"
+    adata.obs[cluster_key] = prior_information["groups_id"]
+
+    # 2. preprocess
+    scv.pp.filter_and_normalize(adata)
+    sc.pp.neighbors(adata, n_neighbors=10)
+    sc.tl.diffmap(adata)
+
+    # 3. execute method
+    sc.tl.paga(adata, groups=cluster_key)
+    # set start porint for dpt
+    adata.uns["iroot"] = np.where(adata.obs.index == start_id)[0][0]
+    sc.tl.dpt(adata, n_dcs=2)
+
+    # 4. extract results
+    # (1) parameters for results extracting
+    epsilon = 1e-3  # a very small scaling values
+    branch_ids = adata.obs[cluster_key].unique().to_list()
+    # (2) branches
+    branches = pd.DataFrame({
+        "branch_id": branch_ids,
+        "directed": True,
+    })
+    branches["length"] = adata.obs[[cluster_key, "dpt_pseudotime"]].groupby(cluster_key).apply(lambda x: x["dpt_pseudotime"].max() - x["dpt_pseudotime"].min() + epsilon).reset_index()[0]
+    # (3) branch_network
+    branch_network = pd.DataFrame(
+        np.triu(adata.uns["paga"]["connectivities"].todense(), k=0),  # keep the upper triangular matrix
+        index=adata.obs[cluster_key].cat.categories,
+        columns=adata.obs[cluster_key].cat.categories
+    ).stack().reset_index()
+    branch_network.columns = ["from", "to", "length"]
+    branch_network = branch_network[branch_network["length"] >= connectivity_cutoff]  # set threshold to filter insignificant edges
+    average_pseudotime_dict = adata.obs.groupby(cluster_key)["dpt_pseudotime"].mean()
+
+    def modify_milestone_network_direction(x):
+        if average_pseudotime_dict[x["from"]] <= average_pseudotime_dict[x["to"]]:
+            return x
+        else:
+            x["from"], x["to"] = x["to"], x["from"]
+            return x
+    branch_network.apply(modify_milestone_network_direction, axis=1)  # Adjust the direction of the edge
+    # sort edges by "from" and "to" columns to facilitate subsequent milestone numbering
+    branch_network["from_pseudotime"] = branch_network["from"].apply(lambda x: average_pseudotime_dict[x])
+    branch_network["to_pseudotime"] = branch_network["to"].apply(lambda x: average_pseudotime_dict[x])
+    branch_network = branch_network.sort_values(["from_pseudotime", "to_pseudotime"])
+    branch_network = branch_network[["from", "to"]].reset_index(drop=True)
+    # (4) branch_progressions
+    branch_progressions = pd.DataFrame({
+        "cell_id": adata.obs.index,
+        "branch_id": adata.obs[cluster_key],
+        "percentage": adata.obs["dpt_pseudotime"]
+    })
+    # sort cells by pseudo time within the branch
+    branch_progressions["percentage"] = branch_progressions.groupby("branch_id")["percentage"].apply(lambda x: (x - x.min()) / (x.max() - x.min() + epsilon)).values
+    branch_progressions
+
+    # 5. save results
+    trajectory_dict = {
+        "branch_network": branch_network,
+        "branches": branches,
+        "branch_progressions": branch_progressions,
+    }
+    return trajectory_dict
+
+
+if __name__ == "__main__":
+    # TODO: for auto docker update, github action script is need in .github directory
+    from parse_args import parse_args
+
+    adata, prior_information, parameters, output_filename = parse_args()
+
+    trajectory_dict = cf_paga(adata, prior_information, parameters)
+
+    with open(output_filename, "wb") as f:
+        pickle.dump(trajectory_dict, f)
+    print("PAGA Finish!")
