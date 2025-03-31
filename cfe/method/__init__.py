52ce68c79f46ff42d3505d1ce81e7590af1eb3ec
黄照样 <554598474@qq.com>
2025-03-31 19:55:12 +0800

    test baseline method

--- cfe/method/__init__.py
+++ cfe/method/__init__.py
@@ -3,7 +3,7 @@ from .fate_method import FateMethod
 from .fate_dynverse_docker_backend import DynverseDockerBackend
 from .fate_cfe_docker_backend import CFEDockerBackend
 from .fate_function_backend import FunctionBackend
-from .function.cf_paga import cf_paga
+from .function import cf_paga, cf_comp1, cf_angle, cf_state_comp, cf_cluster_mst, cf_projection_mst, cf_graph_mst, cf_scvelo
 
 
 __all__ = [
@@ -13,4 +13,10 @@ __all__ = [
     "CFEDockerBackend",
     "FunctionBackend",
     "cf_paga",
+    "cf_comp1",
+    "cf_angle",
+    "cf_state_comp",
+    "cf_cluster_mst",
+    "cf_projection_mst",
+    "cf_scvelo",
 ]
---
+++ tests/method/function/test_cf_projection_mst.py
@@ -0,0 +1,23 @@
+import pytest
+import cfe
+
+import os
+import scanpy as sc
+
+
+class TestCFProjectionMST():
+    def setup_method(self):
+        adata = sc.read_h5ad(f"{os.path.dirname(__file__)}/../../data/bifurcating.h5ad")
+        self.fadata = cfe.data.FateAnnData.from_anndata(adata)
+        self.fadata.obs.index = self.fadata.obs["cell_id"].tolist()
+
+    def test_projection_mst(self):
+        # add priority and parameeters
+        prior_information = {}
+        parameters = {"ndim": 2, "distance_metric": "euclidean"}
+        trajectory_dict = cfe.method.cf_projection_mst(self.fadata, prior_information, parameters)  # add parameters when inferring trajectory
+        assert trajectory_dict.keys() == {"milestone_network", "X_emb", "milestone_emb"}
+
+
+if __name__ == "__main__":
+    pytest.main(["-v", __file__])
