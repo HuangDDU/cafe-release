b6fe828a6172defbeb2101415c7618e78289da11
黄照样 <554598474@qq.com>
2025-07-18 17:49:41 +0800

    update pytest config

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -6,6 +6,7 @@ import cfe
 def test_settings():
     backend = cfe.settings["backend"]
     assert backend in [None, "python_function", "cfe_docker", "dynverse_docker"]
+    # assert backend in [None, "python_function", "conda", "cfe_docker", "dynverse_docker"]
 
 
 if __name__ == "__main__":
