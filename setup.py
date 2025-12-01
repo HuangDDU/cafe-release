e6ba5914f83d3c1e4fd5556b7903c9f7f231c207
HuangDDU WSL <hzy554598474@163.com>
2025-12-01 19:06:43 +0800

    version 0.1.3 test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="cafe-release",
-    version="0.2.0",
+    version="0.1.3",
     author="Zhaoyang Huang",
     author_email="hzy554598474@163.com",
     description="A command line interface for Cafe (Cellular Fate Explorer) project",
